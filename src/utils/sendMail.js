09c16fa9d548340472f7501cf99fc08ce89793f1
Andrii Lushpak <lushpak0111@gmail.com>
2026-06-07 12:14:34 +0300

    Rename sendMail function to sendEmail

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -9,6 +9,6 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendMail = async (options) => {
-  await transporter.sendMail(options);
+export const sendEmail = async (options) => {
+  return await transporter.sendEmail(options);
 };
