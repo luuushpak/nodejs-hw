5652acadde471ea5a4fd32f2a47822a846b24fb9
Andrii Lushpak <lushpak0111@gmail.com>
2026-06-07 12:13:40 +0300

    Rename sendMail to sendEmail in authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,7 +7,7 @@ import jwt from 'jsonwebtoken';
 import handlebars from 'handlebars';
 import path from 'node:path';
 import fs from 'node:fs/promises';
-import { sendMail } from '../utils/sendMail.js';
+import { sendMail } from '../utils/sendEmail.js';
 
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
@@ -129,7 +129,7 @@ export const requestResetEmail = async (req, res) => {
   });
 
   try {
-    await sendMail({
+    await sendEmail({
       from: process.env.SMTP_FROM,
       to: email,
       subject: 'Reset your password',
